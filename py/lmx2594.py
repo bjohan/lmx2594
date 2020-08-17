d4e34066ef077706df9a5f7053a39e2e93b96e93
bjohan <johan.o.bodin@gmail.com>
2020-08-17 20:03:22 +0200

    Librarized code slightly

--- py/lmx2594.py
+++ py/lmx2594.py
@@ -2,6 +2,44 @@ import serial
 import sys
 import time
 import numpy as np
+
+class HardwareInterface:
+    def __init__(self):
+        self.regShadow = {}
+
+
+    def read(self, ra):
+        if self.regShadow[0]&0x4:
+            self.write(0, self.regShadow[0]-0x4)
+        v = self.readReg(ra)
+
+        self.regShadow[ra] = v
+        return v
+    
+    def write(self, ra, v):
+        self.writeReg(ra, v);
+        self.regShadow[ra] = v
+
+    def chipEnable(self, state):
+        print("Chip enable not yet implemented");
+
+    def writeReg(self, ra, v):
+        print("Unimplemented writeReg");
+
+    def readReg(self, ra):
+        print("Unimplemented readReg");
+
+    def reset(self):
+        self.chipEnable(False)
+        time.sleep(0.01)
+        self.chipEnable(True)
+        self.write(0, 1)
+        time.sleep(0.01)
+        self.write(0, 0)
+
+
+
+
 class ArduinoInterface:
     def __init__(self, port, baud):    
         self.s = serial.Serial(port, 115200, timeout = 1)
@@ -80,7 +118,7 @@ class ArduinoInterface:
 
 
 
-class Lmx2594(ArduinoInterface):
+class Lmx2594:
 
     registerMap = {
         0: {"RAMP_EN": [15], "VCO_PHASE_SYNC": [14], "OUT_MUTE": [9], "FCAL_HPFD_ADJ":range(7,9), "FCAL_LPFD_ADJ":range(5,7), "FCAL_EN": [3], "MUX_OUT_LD_SEL":[2], "RESET":[1], "POWERDOWN":[0]},
@@ -123,8 +161,9 @@ class Lmx2594(ArduinoInterface):
 
 
 
-    def __init__(self, port, fosc=100e6):
-        ArduinoInterface.__init__(self, port, 115200)
+    def __init__(self, interface, fosc=100e6):
+        self.iface = interface
+        #ArduinoInterface.__init__(self, port, 115200)
         self.fosc=fosc
         print("Created LMX object wit fosc", fosc/1e6)
 
@@ -135,17 +174,17 @@ class Lmx2594(ArduinoInterface):
             r = l.split("0x")[-1]
             a = int(r[0:2],16)
             v = int(r[2:], 16)
-            self.write(a, v)
+            self.iface.write(a, v)
 
     def enableLockDetect(self, state):
         if state:
-            self.write(0, self.read(0)|0x4)
+            self.iface.write(0, self.read(0)|0x4)
         else:
-            self.write(0, self.read(0)&(0xFFFF-0x4))
+            self.iface.write(0, self.read(0)&(0xFFFF-0x4))
 
 
     def readLockDetectStatus(self):
-        return (self.read(110) >>9)&0x3
+        return (self.iface.read(110) >>9)&0x3
 
     def isLocked(self):
         return self.readLockDetectStatus()==2
@@ -177,21 +216,21 @@ class Lmx2594(ArduinoInterface):
         n = int(factor)
         num = int((factor-n)*0xFFFFFFFF)
         den = 0xFFFFFFFF
-        self.write(36, n);
-        self.write(38, den >> 16)
-        self.write(39, den & 0xFFFF)
-        self.write(42, num >> 16)
-        self.write(43, num & 0xFFFF)
-        self.write(0, self.read(0)|0x8)
+        self.iface.write(36, n);
+        self.iface.write(38, den >> 16)
+        self.iface.write(39, den & 0xFFFF)
+        self.iface.write(42, num >> 16)
+        self.iface.write(43, num & 0xFFFF)
+        self.iface.write(0, self.read(0)|0x8)
 
         if chdiv is not None:
             if chdiv == 0:
                 self.assignBit(31, 0, 14)
             else:
                 self.assignBit(31, 1, 14)
-            rv = self.read(75)
+            rv = self.iface.read(75)
             rv = rv &0xF807| (chdiv<<6)
-            self.write(75, rv)
+            self.iface.write(75, rv)
             #print("reg75 %x"%(rv))
             self.assignBit(45, 0, 11)
             self.assignBit(45, 0, 12)
@@ -207,33 +246,33 @@ class Lmx2594(ArduinoInterface):
             print("Actual frequency vco: ", self.getFpd()*(n+num/float(den)))
         
     def assignBit(self, ra, val, bit):
-        rv = self.read(ra)
+        rv = self.iface.read(ra)
         mask = list('1'*16)
         mask[15-bit]='0'
         #print(mask)
         bm = int(''.join(mask),2)
         #print("mask %x"%(bm))
         rv = rv&bm|(val<<bit)
-        self.write(ra, rv)
+        self.iface.write(ra, rv)
 
 
     def setPower(self, level):
-        rv = self.read(44)
+        rv = self.iface.read(44)
         level = level&0x3F
         rv = (rv & 0xFF)|(level <<8)
-        self.write(44, rv)
+        self.iface.write(44, rv)
 
     def setPowerB(self, level):
         self.assignBit(44, 0, 7)
-        rv = self.read(45);
+        rv = self.iface.read(45);
         rv = rv & 0xFFC0
         rv = rv | (level&0x3F) << 2;
-        self.write(45, rv)
+        self.iface.write(45, rv)
 
         rv = self.read(46);
         rv = rv & 0xFFFC;
         rv = rv | 1;
-        self.write(46, rv)
+        self.iface.write(46, rv)
 
 
     def getAllFieldNames(self):
@@ -275,7 +314,7 @@ class Lmx2594(ArduinoInterface):
 
     def getField(self, name):
         reg, fieldBits = self.findField(name);
-        vBits = self.toBinString(self.read(reg))
+        vBits = self.toBinString(self.iface.read(reg))
         bitString = self.extractIndices(vBits, fieldBits)
         #print("vbits", vBits)
         #print("bitstring", bitString)
@@ -285,11 +324,13 @@ class Lmx2594(ArduinoInterface):
         reg, fieldBits = self.findField(name)
         if len('{0:b}'.format(value)) > len(fieldBits):
             raise ValueError("Value %d is larger than what fits in %d bits"%(value, len(fieldBits)))
-        vBits = self.toBinString(self.read(reg))
+        vBits = self.toBinString(self.iface.read(reg))
         vBitsNew = self.insertIndices(vBits, self.toBinString(value), fieldBits)
         #print(vBits, vBitsNew)
-        self.write(reg, int(vBitsNew[::-1], 2))
+        self.iface.write(reg, int(vBitsNew[::-1], 2))
 
+    def reset(self):
+        self.iface.reset()
         
 
 
@@ -301,59 +342,61 @@ def getIndices(l, i):
         o+=l[j]
     return o
 
-print("Creating LMX obect")
-lmx = Lmx2594(sys.argv[1], fosc=320e6)
-print("Resetting LMX")
-lmx.reset()
-print("Applying config")
-lmx.applyConfig('10GOut320MRef.txt')
-#time.sleep(1000)
-#quit()
-#lmx.setField('OSC_2X', 0)
-#lmx.setField('MULT', 1)
-#lmx.setField('PLL_R', 1)
-#lmx.setField('PLL_R_PRE', 2)
-#lmx.setField('OUT_MUTE', 0)
-#lmx.setField('OUT_FORCE', 1)
-#lmx.setField('MASH_ORDER', 0)
-lmx.setField('OUTB_PD', 0)
-lmx.setField('OUTA_MUX', 1)
-lmx.setField('OUTB_MUX', 1)
-time.sleep(0.1)
-lmx.setFrequency(10e9)
-time.sleep(0.1)
-print("Is locked: ", lmx.isLocked())
-lmx.enableLockDetect(True)
-for fn in lmx.getAllFieldNames():
-    v = lmx.getField(fn)
-    print("%s:"%(fn), v)
-time.sleep(10)
-#for i in range(112):
-#   regdata = lmx.read(i)
-#    if i in registerMap:
-#        print(i, "contains %d named fields"%(len(registerMap[i])))
-#        regBits = bin(regdata)[2:]
-#        regBits = ("0"*(16-len(regBits))+regBits)[::-1]
-#        print("regbits length", len(regBits));
-#        for f in registerMap[i]:
-#            bits = registerMap[i][f]
-#            #print(regBits, bits)
-#            print(f, getIndices(regBits, bits), int(getIndices(regBits, bits),2), end='      ')
-#        else:
-#            print()
-#        print('*'*10)
-#exit()
-#lmx.enableLockDetect(True)
-print("Is locked: ", lmx.isLocked())
-#lmx.enableLockDetect(True)
-#lmx.setPower(8*2*0+31)
-#lmx.setPowerB(8*2*0+31)
-#freqRange = np.linspace(0.01, 1.8, 501)
-freqRange = np.linspace(0.01, 15, 1001)[::-1]
-for f in freqRange:#np.linspace(1.7, 15, 501):
-    lmx.setFrequency(f*1e9)
-    print("rb_LD_VTUNE", lmx.getField("rb_LD_VTUNE"))
-    #print("Is locked: ", lmx.isLocked())
+
+if __name__ == '__main__':
+    print("Creating LMX obect")
+    lmx = Lmx2594(sys.argv[1], fosc=320e6)
+    print("Resetting LMX")
+    lmx.reset()
+    print("Applying config")
+    lmx.applyConfig('10GOut320MRef.txt')
+    #time.sleep(1000)
+    #quit()
+    #lmx.setField('OSC_2X', 0)
+    #lmx.setField('MULT', 1)
+    #lmx.setField('PLL_R', 1)
+    #lmx.setField('PLL_R_PRE', 2)
+    #lmx.setField('OUT_MUTE', 0)
+    #lmx.setField('OUT_FORCE', 1)
+    #lmx.setField('MASH_ORDER', 0)
+    lmx.setField('OUTB_PD', 0)
+    lmx.setField('OUTA_MUX', 1)
+    lmx.setField('OUTB_MUX', 1)
+    time.sleep(0.1)
+    lmx.setFrequency(10e9)
+    time.sleep(0.1)
+    print("Is locked: ", lmx.isLocked())
     lmx.enableLockDetect(True)
-    time.sleep(1)
+    for fn in lmx.getAllFieldNames():
+        v = lmx.getField(fn)
+        print("%s:"%(fn), v)
+    time.sleep(10)
+    #for i in range(112):
+    #   regdata = lmx.read(i)
+    #    if i in registerMap:
+    #        print(i, "contains %d named fields"%(len(registerMap[i])))
+    #        regBits = bin(regdata)[2:]
+    #        regBits = ("0"*(16-len(regBits))+regBits)[::-1]
+    #        print("regbits length", len(regBits));
+    #        for f in registerMap[i]:
+    #            bits = registerMap[i][f]
+    #            #print(regBits, bits)
+    #            print(f, getIndices(regBits, bits), int(getIndices(regBits, bits),2), end='      ')
+    #        else:
+    #            print()
+    #        print('*'*10)
+    #exit()
+    #lmx.enableLockDetect(True)
+    print("Is locked: ", lmx.isLocked())
+    #lmx.enableLockDetect(True)
+    #lmx.setPower(8*2*0+31)
+    #lmx.setPowerB(8*2*0+31)
+    #freqRange = np.linspace(0.01, 1.8, 501)
+    freqRange = np.linspace(0.01, 15, 1001)[::-1]
+    for f in freqRange:#np.linspace(1.7, 15, 501):
+        lmx.setFrequency(f*1e9)
+        print("rb_LD_VTUNE", lmx.getField("rb_LD_VTUNE"))
+        #print("Is locked: ", lmx.isLocked())
+        lmx.enableLockDetect(True)
+        time.sleep(1)
 
